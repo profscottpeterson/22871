8cdf64305a1cfafc53d59c02b28c0f23709e43ec
Brandon20190 <55419782+Brandon20190@users.noreply.github.com>
2020-04-06 19:21:43 -0500

    Add files via upload

---
+++ FinalProject/VictoryScreen.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace FinalProject
+{
+    partial class VictoryScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblWin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(71, 343);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(198, 81);
+            this.btnNewGame.TabIndex = 0;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(681, 343);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(198, 81);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblWin
+            // 
+            this.lblWin.AutoSize = true;
+            this.lblWin.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWin.Location = new System.Drawing.Point(65, 125);
+            this.lblWin.Name = "lblWin";
+            this.lblWin.Size = new System.Drawing.Size(814, 76);
+            this.lblWin.TabIndex = 2;
+            this.lblWin.Text = "Congratulations, you Win!!";
+            // 
+            // VictoryScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1057, 512);
+            this.Controls.Add(this.lblWin);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnNewGame);
+            this.Name = "VictoryScreen";
+            this.Text = "VictoryScreen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblWin;
+    }
+}
---
+++ FinalProject/VictoryScreen.cs
@@ -0,0 +1,60 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="VictoryScreen.cs" company="Brandon Biles, Jeng Leng Lee, Nick Schuchard, Travis Lambert.">
+//     Copyright (c) Brandon Biles, Jeng Leng Lee, Nick Schuchard, Travis Lambert. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace FinalProject
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+    public partial class VictoryScreen : Form
+    {
+        public VictoryScreen()
+        {
+            InitializeComponent();
+        }
+
+        ///<summary>
+        /// Created by: Brandon Biles
+        /// Last Edited by: Brandon Biles
+        /// Last Edit date: 4/2/2020 
+        /// Description: Exits out of application.
+        /// </summary>
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Close the application.
+            Application.Exit();
+        }
+
+        ///<summary>
+        /// Created by: Brandon Biles
+        /// Last Edited by: Brandon Biles
+        /// Last Edit date: 4/2/2020 
+        /// Description: Starts a new game.
+        /// </summary>
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            // Hide current window
+            this.Hide();
+            
+            // Create instance of new game and bring it up.
+            Game game = new Game()
+            {
+                Width = 700,
+                Height = 400
+            };
+            game.ShowDialog();
+
+            // Close current window.
+            this.Close();
+        }
+    }
+}
